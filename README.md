16ca94b9828c0fc196ccd4de1b5eb392ca79dfb2
Noëlle Anthony <noelle.d.anthony@gmail.com>
2018-02-20 14:46:45 -0500

    First commit with ananas support

--- README.md
+++ README.md
@@ -1,29 +1,59 @@
-# mastodon-ebooks.py
+# ananas-ebooks
 
-Simple ebooks-style bot for mastodon.py
+Based on [Maple](https://computerfairi.es/@squirrel)'s [mastodon-ebooks.py]().
 
-Inspired by original work by Jess: https://github.com/Jess3Jane/mastodon-ebooks
+This simply takes Maple's work and adapts it for the ananas framework, allowing you to skip the cron jobs and manual posting.
 
-## Improvements over other ebooks bots
+Usage:
 
-* Scrapes only new toots, not entire list of statuses
-* Separates corpus by account, making a multi-account ebooks easier to manage
-* Does not require Heroku or Ruby
-* Cronjob friendly
+1. In the directory where ananas-ebooks is installed, run `pip3 install -r requirements.txt` to make sure you have all the requirements for the bot.
+2. Create a Mastodon account to be the ebooks bot.
+3. In Mastodon, use your bot account to follow each account you want to scrape to create your bot. **DO NOT** follow any accounts you don't want to scrape.
+4. In Mastodon, under Preferences > Development, create a new app for the bot. You can just call it "Ebooks" and leave "Application Website" blank. Do not change the Redirect URI.
+5. On the next screen, click on the name you gave your bot ("Ebooks" or something else). You'll get a client key, client secret, and access token.
+    (Alternately, you can use a script [like this](https://gist.github.com/Lana-chan/b0d937968d22eca6dcd79a0524449f1d) to generate user secrets to be used by the ebooks script.)
+6. Open `config.cfg`. Make the following changes:
+    1. Change `domain` to whatever your bot's instance is.
+    2. Paste your client key after `client_id`, your client secret after `client_secret`, and your access token after `access_token`.
+    3. By default, your bot will not scrape toots that are replies to other users. set `exclude_replies = False` if you want your bot to scrape replies in addition to "raw" toots.
+7. You're done! Type `ananas config.cfg` into your command line and watch your bot spring into action. It will start by scraping each account you've followed, then toot once each hour and look for replies so it can toot back.
 
-### Future improvements?
+If you want to re-scrape toots from your followed accounts, just stop the bot (with Ctrl-C) and start it again; it will check for new toots every time it starts up.
 
-* Use ananas to use streaming to reply to toots instead of an every-minute cronjob
+## What if I already have an ananas bot running and I'd like to run them in tandem?
 
-## Usage
+Just copy `ebooks.py` and `requirements.txt` into that bot's directory, and modify that `config.cfg` to have an `[EBOOKS]` section as described in step 6 above. ananas bots play very nicely with each other.
 
-1. Create a Mastodon account to be the ebooks bot
-2. Follow the account(s) to be used as source material from bot
-3. You can use a script [like this](https://gist.github.com/Lana-chan/b0d937968d22eca6dcd79a0524449f1d) to generate user secrets to be used by the ebooks script
-4. Scrape the users by running: `./mastodon-ebooks.py -s`
-5. Verify the markov works by running locally to console: `./mastodon-ebooks.py -p`
-6. Toot a markovified status! `./mastodon-ebooks.py -t`
+## What if I want my bot to toot at a different time?
 
-You can now set scraping and tooting to run periodically with cronjobs.
+I'd love to be able to include this in the `config.cfg` file, but for various technical reasons it's not possible right now. Instead, you'll have to actually open up `ebooks.py`.
 
-Optionally, you can also run `./mastodon-ebooks.py -r` regularly to check on notifications and reply to mentions.
+In `ebooks.py`, go to line `129`. You'll see this:
+
+    @hourly(minute=0)
+
+If you want your bot to post once an hour, but not **on** the hour, change the value of `minute`. For example, if you want your bot to post at 12:15, 1:15, 2:15, etc., change it to `minute=15`.
+
+If you want your bot to post **more** than once an hour, just add another line. For example:
+
+    @hourly(minute=12)
+    @hourly(minute=42)
+    def toot(self):
+      ...
+
+will post every half-hour, at XX:12 and at XX:42.
+
+If you want your bot to post **less** than once an hour, change `@hourly` to `@daily`. By default, this will make it post once a day, but like `@hourly`, you can stack them up. To post every four hours, you might do:
+
+    @daily(hour=0, minute=15)
+    @daily(hour=4, minute=15)
+    @daily(hour=8, minute=15)
+    @daily(hour=12, minute=15)
+    @daily(hour=16, minute=15)
+    @daily(hour=20, minute=15)
+    def toot(self):
+      ...
+
+Note how that uses 24-hour time.
+
+You can see more details at the [ananas repo](https://github.com/chr-1x/ananas).
---
+++ ebooks.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+from mastodon import Mastodon
+from bs4 import BeautifulSoup
+import markovify
+import html
+import json
+import os
+import sys
+import getopt
+from ananas import PineappleBot, hourly, schedule, reply, html_strip_tags
+
+class ebooksBot(PineappleBot):
+  exclude_replies = True
+
+  def start(self):
+    try:
+      self.exclude_replies = bool(self.config.exclude_replies)
+    except:
+      self.exclude_replies = True
+    self.scrape()
+
+  # strip html tags for text alone
+  def strip_tags(self, content):
+    soup = BeautifulSoup(html.unescape(content), 'html.parser')
+    # remove mentions
+    tags = soup.select('.mention')
+    for i in tags:
+      i.extract()
+    # clear shortened link captions
+    tags = soup.select('.invisible, .ellipsis')
+    for i in tags:
+      i.unwrap()
+    # replace link text to avoid caption breaking
+    tags = soup.select('a')
+    for i in tags:
+      i.replace_with(i.get_text())
+    # strip html tags, chr(31) joins text in different html tags
+    return soup.get_text(chr(31)).strip()
+
+  # scrapes the accounts the bot is following to build corpus
+  def scrape(self):
+    me = self.mastodon.account_verify_credentials()
+    following = self.mastodon.account_following(me['id'])
+    acctfile = 'accts.json'
+    # acctfile contains info on last scraped toot id
+    try:
+      with open(acctfile, 'r') as f:
+        acctjson = json.load(f)
+    except:
+      acctjson = {}
+    
+    print(acctjson)
+    for acc in following:
+      id = str(acc['id'])
+      try:
+        since_id = self.scrape_id(id, since=acctjson[id])
+      except:
+        since_id = self.scrape_id(id)
+      acctjson[id] = since_id
+    
+    with open(acctfile, 'w') as f:
+      json.dump(acctjson, f)
+      
+    # generate the whole corpus after scraping so we don't do at every runtime
+    combined_model = None
+    for (dirpath, _, filenames) in os.walk("corpus"):
+      for filename in filenames:
+        with open(os.path.join(dirpath, filename)) as f:
+          model = markovify.NewlineText(f, retain_original=False)
+          if combined_model:
+            combined_model = markovify.combine(models=[combined_model, model])
+          else:
+            combined_model = model
+    with open('model.json','w') as f:
+      f.write(combined_model.to_json())
+
+  def scrape_id(self, id, since=None):
+    # excluding replies was a personal choice. i haven't made an easy setting for this yet
+    toots = self.mastodon.account_statuses(id, since_id=since, exclude_replies=self.exclude_replies)
+    # if this fails, there are no new toots and we just return old pointer
+    try:
+      new_since_id = toots[0]['id']
+    except:
+      return since
+    bufferfile = 'buffer.txt'
+    corpusfile = 'corpus/%s.txt' % id
+    i = 0
+    with open(bufferfile, 'w') as output:
+      while toots != None:
+        # writes current amount of scraped toots without breaking line
+        i = i + len(toots)
+        sys.stdout.write('\r%d' % i)
+        sys.stdout.flush()
+        filtered_toots = list(filter(lambda x: x['spoiler_text'] == "" and x['reblog'] is None and x['visibility'] in ["public", "unlisted"], toots))
+        for toot in filtered_toots:
+          output.write(html_strip_tags(toot['content'])+'\n')
+        toots = self.mastodon.fetch_next(toots)
+      # buffer is appended to the top of old corpus
+      if os.path.exists(corpusfile):
+        with open(corpusfile,'r') as old_corpus:
+          output.write(old_corpus.read())
+      directory = os.path.dirname(corpusfile)
+      if not os.path.exists(directory):
+        os.makedirs(directory)
+      os.rename(bufferfile,corpusfile)
+      sys.stdout.write('\n')
+      sys.stdout.flush()
+    return new_since_id
+
+  # returns a markov generated toot
+  def generate(self, length=None):
+    modelfile = 'model.json'
+    if not os.path.exists(modelfile):
+      sys.exit('no model -- please scrape first')
+    with open(modelfile, 'r') as f:
+      reconstituted_model = markovify.Text.from_json(f.read())
+    if length:
+      msg = reconstituted_model.make_short_sentence(length)
+    else:
+      msg = reconstituted_model.make_sentence()
+    return msg.replace(chr(31), "\n")
+
+  # perform a generated toot to mastodon
+  @hourly(minute=0)
+  def toot(self):
+    msg = self.generate(500)
+    self.mastodon.toot(msg)
+    print('Tooted: %s' % msg)
+
+  # scan all notifications for mentions and reply to them
+  @reply
+  def post_reply(self, mention, user):
+    msg = html_strip_tags(mention["content"])
+    rsp = self.generate(400)
+    tgt = user["acct"]
+    irt = mention["id"]
+    vis = mention["visibility"]
+    print("Received toot from {}: {}".format(tgt, msg))
+    print("Responding with {} visibility: {}".format(vis, rsp))
+    final_rsp = "@{} {}".format(tgt, rsp)
+    final_rsp = final_rsp[:500]
+    self.mastodon.status_post(final_rsp,
+                  in_reply_to_id = irt,
+                  visibility = vis)
